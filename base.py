eb3d20598fe61b238ed4d72f5cb9bcffa9dddc2f
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2025-08-26 11:53:18 +0000

    identação

--- base.py
+++ base.py
@@ -1,14 +1,13 @@
-receitas = 
-    {
+receitas = {
         'nome': 'pipoca doce',
         'ingredientes': ['5 colheres(sopa) de oleo', '5 colheres(sopa) de milho para pipoca', '5 colheres(sopa) de açucar', '3 colheres(sopa) de agua', '1 colher(sopa) de chocolate em po'],
         'utensilios': ['panela', 'bowl', 'espátula', 'pipoqueira'],
         'modo de preparo': ['Em uma panela, adicione todos os ingredientes e misture delicadamente.', 'Desligue o fogo quando o intervalo de tempo entre os estouros da pipoca diminuir.'],
 
-    },
-    {
+    },{
         'nome': 'torta de limao',
         'ingredientes': ['200 g de biscoito de maisena', '150g de margarina', '1 caixa de creme de leite (200 g)', 'suco de 4 limões', 'raspas de 2 limões', '3 ou 4 claras de ovo', '3 colheres (sopa) de açúcar', 'raspas de 2 limões para decorar'],
         'utensilios': ['Assadeira', 'liquidificador', 'fouet', 'colher para sobremesa'],
         'modo de preparo': ['Triture o biscoito de maisena em um liquidificador ou processador.', 'Junte a margarina e bata mais um pouco.', 'Despeje a massa em uma forma de fundo removível (27 cm de diâmetro).', 'Com as mãos, espalhe os biscoitos triturados no fundo e nas laterais da forma, cobrindo toda área de maneira uniforme.', 'Leve ao forno médio (180° C), preaquecido, por aproximadamente 10 minutos.']
     }
+
