0ab802b13e4d32414f6b639ffa87f6aaf1f8e7ac
Noan <noan.guedes06@aluno.ifce.edu.br>
2025-11-11 11:56:44 +0000

    finalização

---
+++ test.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import session
+
+from models import User, table_registry
+
+app = FastAPI(title = 'API de teste')
+
+engine = create_engine("sqlite:///:memory:", echo=False)
+
+table_registry.metadata.create_all(engine)
+
+with Session(engine) as session:
+    noan = user(
+        username = "noan", password = "senha123", email = "noan@gmail.com"
+    )
+    session.add(noan)
+    session.commit()
+    session.refresh(noan)
+
+print("DADOS DO USUARIO:", noan)
+print("ID:", noan.id)
+print("Criado em:", noan.created_at)    
