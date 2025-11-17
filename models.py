a2699301d0994a781a921b1bcefa3d10cfc91589
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2025-11-17 14:01:50 +0000

    Migrações

--- models.py
+++ models.py
@@ -14,5 +14,11 @@ class User:
     senha: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
-        init=False, server_default=func.now
+        init=False, server_default=func.now()
     )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False,                
+        server_default=func.now(), 
+        onupdate=func.now()
+    )
+    
